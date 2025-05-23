b09b24ae12f7fe519854c9489992683e4d1347ae
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-23 16:43:47 +0000

    init

--- answer.php
+++ answer.php
@@ -9,7 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-yellow.min.css" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.purple-deep_orange.min.css" />
   <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
@@ -19,7 +19,6 @@
 
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <script src="./js/script.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
@@ -28,20 +27,26 @@
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/volume of sphere.png" alt="sphere image" />
+        <img src="./images/volume of sphere.png" width="250" />
       </div>
-      <br />
-      <form action="answer.php" method="GET">
-        <div class="page-content">
-          <h2>Formula</h2>
-          <h3>V = (4/3) x π x r³</h3>
-          <br />
-          <br />
-          <div class="page-content-answer">
-            The radius is:
-            <div name="dimension"></div>
-            <div name="volume"></div>
-          </div>
+      <div class="page-content-php">
+        <?php
+        // this calculates volume of a sphere
+        $radiusNumber = $_GET["radius-number"];
+
+        // Calculate surface area of the cylinder
+        $volume = (4 / 3) * M_PI * ($radiusNumber ** 3);
+
+        // Output the result
+        echo "If the sphere has a radius = " . $radiusNumber . " mm<br />";
+        echo "<br />";
+        echo "The volume is:" . (round($volume, 2)) . " mm³.";
+        echo "<br />";
+        ?>
+      </div>
+  </div>
+  <div class="page-content-return">
+    <a href="./index.php">Return ...</a>
   </div>
   </main>
   </div>
